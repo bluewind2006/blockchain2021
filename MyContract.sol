14f9e85aeb7d341b865919e3dd044f0216c5920b
bluewind <bluewind2006@gmail.com>
2021-07-15 22:29:30 +0900

    Add files via upload

---
+++ MyContract.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+//https://forum.openzeppelin.com/t/solidity-0-6-8-introduces-spdx-license-identifiers/2859
+pragma solidity 0.8.3;
+
+contract MyContract {
+        function sayHello() public pure returns(string memory) {
+        return "Hello World!";
+    }
+}
