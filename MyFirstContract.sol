b1aa4c3104053e84bb241163990d39bdeb0cc0d5
bluewind <bluewind2006@gmail.com>
2021-09-14 12:49:23 +0900

    Update MyFirstContract.sol

--- MyFirstContract.sol
+++ MyFirstContract.sol
@@ -16,3 +16,25 @@ contract MyFirstContract {
     
     
 }
+
+수정된 버전.
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.8;
+contract MyFirstContract2 {
+    string fName;
+   	uint age;
+   	event Instructor(
+   		string name,
+   		uint age
+	);
+    function setInstructor(string memory _fName, uint _age) public {
+        fName = _fName;
+        age = _age;
+        emit Instructor(_fName, _age);        // Add this
+    }
+    function getInstructor() view public returns (string memory, uint) {
+        return (fName, age);
+    }
+}
