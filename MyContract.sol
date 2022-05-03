a1ef6897723646d1259bf8f9653698b3dcd9f5cd
bluewind <bluewind2006@gmail.com>
2022-05-03 16:59:29 +0900

    Delete MyContract.sol

--- MyContract.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-//https://forum.openzeppelin.com/t/solidity-0-6-8-introduces-spdx-license-identifiers/2859
-pragma solidity 0.8.3;
-
-contract MyContract {
-        function sayHello() public pure returns(string memory) {
-        return "Hello World!";
-    }
-}
